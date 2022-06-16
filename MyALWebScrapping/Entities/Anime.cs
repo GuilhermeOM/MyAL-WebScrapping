803144fa40ae4bf078691c199bf532bba2ec1e3b
GuilhermeOM <guilhermeomalta0@gmail.com>
2022-06-16 05:02:39 -0300

    Scrapping the anime basic info (rank, name and score) per page.

---
+++ MyALWebScrapping/Entities/Anime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyALWebScrapping.Entities
+{
+    public class Anime
+    {
+        public int Rank { get; set; }
+        public string Name { get; set; }
+        public double Score { get; set; }
+
+        public Anime(int rank, string name, double score)
+        {
+            Rank = rank;
+            Name = name;
+            Score = score;
+        }
+    }
+}
--- MyALWebScrapping/Program.cs
+++ MyALWebScrapping/Program.cs
@@ -1,8 +1,12 @@
 ﻿using MyALWebScrapping.Configs;
+using MyALWebScrapping.Entities;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
 
 namespace MyALWebScrapping
 {
@@ -15,9 +19,47 @@ namespace MyALWebScrapping
 
             driver.Navigate().GoToUrl("https://myanimelist.net/topanime.php");
 
-            if (ElementExists(driver, "pb12", 10))
+            if (ElementExists(driver, "top-ranking-table", 10))
             {
-                Console.WriteLine("Exists");
+                try
+                {
+                    int amountOfScrappedData = 0;
+
+                    while (amountOfScrappedData <= 1000)
+                    {
+                        IList<IWebElement> rows = driver.FindElements(By.ClassName("ranking-list"));
+
+                        int rank = 0;
+                        string name = null;
+                        double score = 0;
+
+                        foreach (IWebElement row in rows)
+                        {
+                            rank = Int32.Parse(row.FindElement(By.ClassName("top-anime-rank-text")).Text);
+                            name = row.FindElement(By.ClassName("anime_ranking_h3")).Text;
+                            score = Double.Parse(row.FindElement(By.ClassName("score-label")).Text, CultureInfo.InvariantCulture);
+
+                            Anime anime = new Anime(rank, name, score);
+
+                            Console.WriteLine("RANK: " + anime.Rank);
+                            Console.WriteLine("NAME: " + anime.Name);
+                            Console.WriteLine("SCORE: " + anime.Score);
+                        }
+
+                        amountOfScrappedData += rows.Count;
+
+                        if (!DidNextPaginationLoaded(driver, rank))
+                            break;
+                    }
+                }
+                catch (StaleElementReferenceException error)
+                {
+                    Console.WriteLine(error.StackTrace);
+                }
+                catch (NoSuchElementException error)
+                {
+                    Console.WriteLine(error.StackTrace);
+                }
             }
         }
 
@@ -43,6 +85,31 @@ namespace MyALWebScrapping
             });
 
             return elementExists;
+        }
+
+        private static bool DidNextPaginationLoaded(IWebDriver currentDriver, int lastCheckedRank)
+        {
+            try
+            {
+                currentDriver.FindElement(By.ClassName("next")).Click();
+
+                Thread.Sleep(300);
+
+                int currentTableRank = Convert.ToInt32(currentDriver.FindElement(By.ClassName("top-anime-rank-text")).Text);
+
+                if (currentTableRank > lastCheckedRank)
+                    return true;
+
+                return false;
+            }
+            catch (StaleElementReferenceException)
+            {
+                return false;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
         }
     }
 }
