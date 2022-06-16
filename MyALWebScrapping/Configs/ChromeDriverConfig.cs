602361b4f47faa4e9a28c032efbf832f9f250260
GuilhermeOM <guilhermeomalta0@gmail.com>
2022-06-16 01:38:46 -0300

    Setting up the chrome driver configuration and veryfing if the main table info exists.

---
+++ MyALWebScrapping/Configs/ChromeDriverConfig.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace MyALWebScrapping.Configs
+{
+    public class ChromeDriverConfig
+    {
+        public ChromeOptions options;
+        public ChromeDriverService service;
+
+        public ChromeDriverConfig()
+        {
+            HandleDriverConfiguration();
+        }
+
+        private void HandleDriverConfiguration()
+        {
+            options = new ChromeOptions();
+            options.AddArgument("headless");
+            options.AddArgument("--silent");
+            options.AddArgument("log-level=3");
+
+            service = ChromeDriverService.CreateDefaultService(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            service.HideCommandPromptWindow = true;
+            service.SuppressInitialDiagnosticInformation = true;
+        }
+    }
+}
--- MyALWebScrapping/Program.cs
+++ MyALWebScrapping/Program.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿using MyALWebScrapping.Configs;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
 
 namespace MyALWebScrapping
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ChromeDriverConfig driverConfig = new ChromeDriverConfig();
+            IWebDriver driver = new ChromeDriver(driverConfig.service, driverConfig.options);
+
+            driver.Navigate().GoToUrl("https://myanimelist.net/topanime.php");
+
+            if (ElementExists(driver, "pb12", 10))
+            {
+                Console.WriteLine("Exists");
+            }
+        }
+
+        private static bool ElementExists(IWebDriver currentDriver, string elementId, int timeout)
+        {
+            WebDriverWait wait = new WebDriverWait(currentDriver, TimeSpan.FromSeconds(timeout));
+
+            bool elementExists = wait.Until(condition =>
+            {
+                try
+                {
+                    IWebElement elementToBeDisplayed = currentDriver.FindElement(By.ClassName(elementId));
+                    return elementToBeDisplayed.Displayed;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+
+            return elementExists;
         }
     }
 }
